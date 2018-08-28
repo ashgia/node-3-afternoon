9c8a19d4eb3cfae276ff9df56e0e035c25543c67
Ashley Nguyen <ashleygnguyen@yahoo.com>
2018-08-28 16:38:07 -0500

    completed

--- server/index.js
+++ server/index.js
@@ -2,13 +2,15 @@ const express = require("express");
 const { json } = require("body-parser");
 const session = require("express-session");
 require("dotenv").config();
-const port = 3001;
+const port = 3000;
 
 //middleware
 const checkForSession = require("./middlewares/checkForSession");
 //controllers
 const swag_controller = require("./controllers/swag_controller");
 const auth_controller = require("./controllers/auth_controller");
+const cart_controller = require("./controllers/cart_controller");
+const search_controller = require("./controllers/search_controller");
 
 const app = express();
 
@@ -22,6 +24,7 @@ app.use(
   })
 );
 app.use(checkForSession);
+app.use(express.static(`${__dirname}/build`));
 
 //swag
 app.get("/api/swag", swag_controller.read);
@@ -31,6 +34,14 @@ app.post("/api/login", auth_controller.login);
 app.post("/api/register", auth_controller.register);
 app.post("/api/signout", auth_controller.signout);
 app.get("/api/user", auth_controller.getUser);
+
+//cart
+app.post("/api/cart", cart_controller.add);
+app.post("/api/cart/checkout", cart_controller.checkout);
+app.delete("/api/cart", cart_controller.delete);
+
+//search
+app.get("/api/search", search_controller.search);
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
